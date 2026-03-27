c0da55dedaab0201a86f64ee6f0ef145ad229660
majormike3292-cmd <majormike3292@gmail.com>
2026-03-26 21:06:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ document.getElementById("secret").classList.toggle("hidden");
 // MUSIC
 function playMusic(){
   
+// FLOATING HEARTS
+const heartsContainer = document.getElementById("hearts");
+
+setInterval(()=>{
+let heart = document.createElement("span");
+heart.innerHTML = "💜";
+heart.style.left = Math.random()*100 + "vw";
+heart.style.fontSize = (Math.random()*20+10) + "px";
+heartsContainer.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},10000);
+
+},500);
